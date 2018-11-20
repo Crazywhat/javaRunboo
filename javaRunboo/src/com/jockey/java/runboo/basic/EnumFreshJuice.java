27d42173d66106eaa4d03274f416c877cfa3087a
jockey <641072337@qq.com>
2018-11-20 15:28:56 +0800

    testSubmit

---
+++ javaRunboo/src/com/jockey/java/runboo/basic/EnumFreshJuice.java
@@ -0,0 +1,20 @@
+package com.jockey.java.runboo.basic;
+
+import com.jockey.java.runboo.basic.FreshJuice.FreshJuiceSize;
+
+public class EnumFreshJuice {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FreshJuice juicePlease = new FreshJuice();
+		juicePlease.size = FreshJuiceSize.LARGE;
+		
+		System.out.println(juicePlease.size);
+	}
+
+}
+
+class FreshJuice{
+	enum FreshJuiceSize{SMALL,MEDIUM,LARGE};
+	FreshJuiceSize size;
+}
